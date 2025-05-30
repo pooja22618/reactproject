bc6cf349f8843a8cf9b0d7028c9e393035afd15a
Ketan Anand <22118128007@bpmcemadhepura.org>
2025-05-30 19:52:29 +0530

    first commit

---
+++ src/components/contact.js
@@ -0,0 +1,82 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
+export default function AppContact() {
+  return (
+    <section id="contact" className="block contact-block">
+      <Container fluid>
+        <div className="title-holder">
+          <h2>contact us</h2>
+          <div className="subtitle">get connected with us</div>
+        </div>
+        <Form className="contact-form">
+          <Row>
+            <Col sm={4}>
+              <Form.Control
+                type="text"
+                placeholder="Enter your full name"
+                required
+              />
+            </Col>
+            <Col sm={4}>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                required
+              />
+            </Col>
+            <Col sm={4}>
+              <Form.Control
+                type="tel"
+                placeholder="Enter your contact number"
+                required
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <Form.Control
+                as="textarea"
+                placeholder="Enter your message here..."
+                required
+              />
+            </Col>
+          </Row>
+          <div className="btn-holder">
+            <Button type="submit">Submit</Button>
+          </div>
+        </Form>
+      </Container>
+      <div className="google-map">
+        <iframe
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14361.66490499667!2d85.78260084557796!3d25.855774946462084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed90eea5571da3%3A0x3ea995ba235a88fa!2sProfessor%20Colony%2C%20Bahadurpur%2C%20Bihar%20848101!5e0!3m2!1sen!2sin!4v1748607643234!5m2!1sen!2sin"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <Container fluid>
+        <div className="contact-info">
+          <ul>
+            <li>
+              <i className="fas fa-envelope"></i>
+              poojaroy07102003@gmail.com
+            </li>
+            <li>
+              <i className="fas fa-phone"></i>
+              9304836846
+            </li>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              samastipur, bihar
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </section>
+  );
+}
